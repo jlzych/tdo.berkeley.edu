b2e9c44bdf3ed770d6f9b2bf581c760762b2fbe2
Jeff Zych <jlzych@gmail.com>
2012-07-01 21:39:52 -0700

    Added book section.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 TdoBerkeleyEdu::Application.routes.draw do
+
   root :to => 'home#index'
+
+  match "book" => 'book#index', :as => :book_index
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -47,10 +50,6 @@ TdoBerkeleyEdu::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
