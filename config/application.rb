631f7f456a20b0082b283c921e84c646d4ab2118
Jeff Zych <jlzych@gmail.com>
2012-07-01 21:28:35 -0700

    Added home page.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ end
 
 module TdoBerkeleyEdu
   class Application < Rails::Application
+    config.sass.preferred_syntax = :sass
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
