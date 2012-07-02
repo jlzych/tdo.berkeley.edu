ac81e50ddf5726b8f3186d5bf95b9e555300445c
Jeff Zych <jlzych@gmail.com>
2012-07-01 21:06:59 -0700

    Initial commit. Empty rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TdoBerkeleyEdu::Application.config.secret_token = 'db958144830ff5d382029631f532fe7755221232a50509b2c53d845f0001e4b8c9a20491a771e17c2924e9319be11d347e12e5fb16025c8889327a4c58772b50'
