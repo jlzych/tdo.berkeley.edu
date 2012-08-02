d8decbc4ec191347f8eb39d270b8c2d25ec7574e
Jeff Zych <jlzych@gmail.com>
2012-08-01 22:53:31 -0700

    Turned on history in rails_admin.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -29,7 +29,7 @@ RailsAdmin.config do |config|
   config.current_user_method { current_user } # auto-generated
 
   # If you want to track changes on your models:
-  #config.audit_with :history, User
+  config.audit_with :history, User
 
   # Or with a PaperTrail: (you need to install it first)
   # config.audit_with :paper_trail, User
