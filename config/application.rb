1737a2c078c3b5ed4bc7fb59792041ff38e48ff6
Jeff Zych <jlzych@gmail.com>
2012-08-17 18:54:50 -0700

    Changed app to precompile book.js.coffee.

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,7 @@ module TdoBerkeleyEdu
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['book.js']
   end
 end
